6c060527acf783aa5ce2195c40a10017f625ba57
Koussay Dhifi <koussaydhiti48@gmail.com>
2022-08-21 18:26:23 +0100

    Remade the game with more logic

--- game.cpp
+++ game.cpp
@@ -2,116 +2,179 @@
 #include <string>
 using namespace std;
 
+class Animat {
 
-class Animatronic {
 	public:
-		Animatronic (string nameS, double rageA) {
-			name = nameS;
-			rage = rageA;
+		Animat (string name, double rage, int noj) {
+			name = name;
+			rage = rage;
+			noj = noj;
+			if (rage<2.5) {
+				position = 1;
+
+			}else if (rage>=2.5 && rage<3.5) {
+				position = 2;
+			}else {
+				position = 3;
+			}
 		}
 
-		string getName() {
+		int getPos () {
+			return position;
+		}
+
+		int getNoj () {
+			return noj;
+
+		}
+
+		string getName () {
 			return name;
 		}
 
-		double getRage() {
-			return rage;
 
-		}
-
-		void incRage(double r) {
+		void IncRage(double r) {
 			rage = rage+r;
 		}
 
-		string getPlace() {
-			if (((rage*2)/7)>=1) {
-				if (name == "Chicka") {
-					return "Chicka is on the Right Door \n \n";
-				}else if (name == "Bonnie") {
-					return "Bonnie is on the Left Door \n \n";
-				}else if (name == "Freddy") {
-					return "Freddy is Close \n \n";
-				}
-			}else if (((rage*2)/7)<1 && ((rage*2)/7) > 0.5) {
-				if (name == "Chicka") {
-					return "Chicka is in the Kitchen \n \n";
-				}else if (name == "Bonnie") {
-					return "Bonnie is in the Staff Room \n \n";
-				}else if (name == "Freddy") {
-					return "Freddy is in the party place \n \n";
-				}
-			}else {
-				if (name == "Chicka") {
-					return "Chicka is in the theatre \n \n";
-				}else if (name == "Bonnie") {
-					return "Bonnie is in the theatre \n \n";
-				}else if (name == "Freddy") {
-					return "Freddy is in the theatre \n \n";
-				}
-			}
-		}
+
 
 	private:
 		string name;
 		double rage;
+		int noj;
+		int position;
 };
 
 
-
-
-
 int main () {
-	int night = 1;
+	//Declaring Animatronics
+	Animat chicka ("Chicka", 1.5, 3);
+
+	Animat bonnie ("Bonnie", 1, 2);
+
+	//Main variables
 	int answer;
-	bool start = true;
-	bool win,lost = false;
-	Animatronic freddy("Freddy",1);
-	Animatronic Chicka ("Chicka",1.5);
-	Animatronic Bonnie ("Bonnie",1.5);
+	int night = 1;
+	int power = 100;
+	int time = 1;
+	bool left_door = true;
+	bool right_door = true;
+	bool win = false;
+	bool lost = false;
 
-
+	//Game Loop
 	while (!(win || lost)) {
-		if (start) {
-			cout << "Welcome to: \n Five Nights at freddy's \n";
-			cout << "1-Start Game(Night " << night << ")\n";
-			cout << "2-Quit Game \n";
-			cin >> answer;
-			if (answer == 1) {
-				start = false;
+		//Menu
+		cout << "Five Night's at Bonnie&&Chicka: \n ";
+		cout << "1- Play Game (Night 1) \n";
+		cout << "2-Quit Game \n";
+		cin >> answer;
 
-			}else {
+		switch (answer) {
+			case 2:
+				win = true;
 				break;
-			}
-		}else {
-			cout << "1-Check Right Door \n 2-Check left Door \n 3-Check Camera \n 4-Close right Door \n 5-Close Left Door \n";
-			cin >> answer;
-			switch (answer) {
-				case 1:
-					if (Chicka.getPlace() == "Chicka is on the Right Door \n \n") {
-						cout << "Chicka is on the Right Door... \n";
+			case 1:
+				//Game
+				while (! (win || lost))
+				{
+				cout << "Night: " << night << endl;
+				cout << "Time: " << time << "AM" << endl;
+				cout << "Power: " << power << "%" << endl;
 
-					}
-					break;
-				case 2:
+				cout << "1-Check Camera 1 \n";
+				cout << "2-Check Camera 2 \n";
+				if (right_door) {
+					cout << "3-Close Right Door \n";
 
-					break;
-				case 3:
-					cout << Chicka.getPlace() << endl;
-					cout << Chicka.getRage() << endl;
-					break;
+				}else if (! right_door) {
+					cout << "3-Open Right Door \n";
+				}
+				if (left_door) {
+					cout << "4-Close Left Door \n";
 
-				case 4:
-					if (Chicka.getPlace() == "Chicka is on the Right Door") {
-						Chicka.incRage(-0.5);
+				}else if (! left_door) {
+					cout << "4-Open Left Door \n";
+				}
 
-					}else {
-						Chicka.incRage(0.5);
-					}
-					break;
+				cout << "5-Check Right door \n";
+				cout << "6-Check left Door \n";
+				cout << "7-Do nothing \n";
+				cin >> answer;
 
-				case 5:
-					break;
-			}
+				//Answers
+				switch (answer) {
+					case 1:
+						if (chicka.getPos() == 1) {
+							cout << "Chicka is on Camer 1 (Far) \n";
+						}else {
+							cout << "Chicka is not on camera 1 \n";
+						}
+						if (bonnie.getPos() == 1) {
+							cout << "Bonnie is on camera 1 (Far) \n";
+						}else {
+							cout << "Bonnie is not on camera 1 \n";
+						}
+						power -= 5;
+						break;
+					case 2 :
+						if (chicka.getPos() == 2) {
+							cout << "Chicka is on Camer 2 (Far) \n";
+						}else {
+							cout << "Chicka is not on camera 2 \n";
+						}
+						if (bonnie.getPos() == 2) {
+							cout << "Bonnie is on camera 2 (Far) \n";
+						}else {
+							cout << "Bonnie is not on camera 2 \n";
+						}
+						power -= 5;
+						break;
+
+					case 3:
+						if (right_door) {
+							right_door = false;
+						}else {
+							right_door = true;
+						}
+						break;
+					case 4:
+						if (left_door) {
+							left_door = false;
+						}else {
+							left_door = true;
+						}
+						break;
+
+					case 5:
+						if (chicka.getPos() == 3) {
+							cout << "Chicka is on the right door (Super Close) \n";
+						}else {
+							cout << "No one is here \n";
+						}
+						break;
+					case 6:
+						if (bonnie.getPos() == 2) {
+							cout << "Bonnie is on the left door \n";
+						}else {
+							cout << "No one is here \n";
+						}
+						break;
+					default:
+						continue;
+						break;
+
+
+					} //End of switch statement
+
+
+
+
+				}
+				break;
+
 		}
 	}
+
 }
